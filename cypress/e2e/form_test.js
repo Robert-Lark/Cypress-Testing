0871a3b7efe361213a3345bb1ab10f287e796b84
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-07-16 18:12:45 -0700

    completed mvp

---
+++ cypress/e2e/form_test.js
@@ -0,0 +1,25 @@
+describe("Form Test", () => {
+	it("is form working", () => {
+		cy.visit("http://localhost:3000/");
+		cy.get(':nth-child(7) > input')
+		.type('pass')
+		.clear('pass')
+		cy.get('.error')
+			.should('have.value', '')
+		cy.get(':nth-child(1) > input')
+			.type('Rob Lark')
+			.should('have.value', 'Rob Lark')
+		const email = 'robert.lark87@gmail.com'
+		cy.get(':nth-child(4) > input')
+			.type(email)
+			.should('have.value', email)
+		cy.get(':nth-child(7) > input')
+			.type('password')
+			.should('have.value', 'password')
+		cy.get('.terms > input')
+			.click()
+			.should('have.checked', true)
+		cy.get('button')
+			.should('not.be.disabled')
+	});
+});
